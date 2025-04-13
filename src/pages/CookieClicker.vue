ed6b2c5d26590e9efc11e65cb56552b6712fcaba
markusmikk100 <TauriTammela272@gmail.com>
2025-04-13 14:04:03 +0300

    Added click buffs, img changes when clicked, sounds

--- src/pages/CookieClicker.vue
+++ src/pages/CookieClicker.vue
@@ -1,49 +1,104 @@
 <script setup>
-import { computed, onUnmounted, ref } from 'vue';
-
+import { computed, onUnmounted, ref } from "vue";
+import img1 from "../assets/Cookie1.png";
+import img2 from "../assets/Cookie2.png";
+import img3 from "../assets/Cookie3.png";
+import cunchSound from "../assets/Cunch.mp3";
+let currentImage = ref(img1);
 let cookies = ref(0);
 
 let buildings = ref([
-    { name: 'Cursor', price: 15, count: 0, cps: 0.1 },
-    { name: 'Grandma', price: 100, count: 0, cps: 1 },
-    { name: 'Farm', price: 1000, count: 0, cps: 10 },
-    { name: 'Factory', price: 10000, count: 0, cps: 100 },
+	{ name: "Cursor", price: 15, count: 0, cps: 0.1 },
+	{ name: "Grandma", price: 100, count: 0, cps: 1 },
+	{ name: "Farm", price: 1000, count: 0, cps: 10 },
+	{ name: "Factory", price: 10000, count: 0, cps: 100 },
+]);
+
+let buffs = ref([
+	{ name: "Little click", price: 15, count: 0, cpc: 0.1 },
+	{ name: "DB click", price: 100, count: 0, cpc: 2 },
+	{ name: "Super click", price: 1000, count: 0, cpc: 10 },
+	{ name: "MEGA click", price: 10000, count: 0, cpc: 25 },
 ]);
 
 let cps = computed(() => {
-    return buildings.value.reduce((total, building) => total+building.cps*building.count, 0);
+	return buildings.value.reduce(
+		(total, building) => total + building.cps * building.count,
+		0
+	);
+});
+
+let cpc = computed(() => {
+	return buffs.value.reduce((total, buff) => total + buff.cpc * buff.count, 1);
 });
 
 const cpsInterval = setInterval(() => {
-    cookies.value+=cps.value;
-    document.title = `🍪 ${cookies.value} cookies`;
+	cookies.value += cps.value;
+	document.title = `🍪 ${cookies.value} cookies`;
 }, 1000);
 
+function CookieClick() {
+	cookies.value += cpc.value;
 
-function buyBuilding(building){
-    cookies.value-=building.price;
-    building.price += Math.ceil(building.price * 0.15);
-    building.count++;
+	const audio = new Audio(cunchSound);
+	audio.play();
+
+	if (currentImage.value == img1) {
+		currentImage.value = img2;
+	} else if (currentImage.value == img2) {
+		currentImage.value = img3;
+	} else {
+		currentImage.value = img1;
+	}
+}
+
+function buyBuilding(building) {
+	cookies.value -= building.price;
+	building.price += Math.ceil(building.price * 0.15);
+	building.count++;
+}
+
+function buyBuff(buff) {
+	cookies.value -= buff.price;
+	buff.price += Math.ceil(buff.price * 0.15);
+	buff.count++;
 }
 
 onUnmounted(() => {
-    clearInterval(cpsInterval);
+	clearInterval(cpsInterval);
 });
 </script>
 <template>
-    <div class="columns">
-        <div class="column is-3 has-background-primary has-text-centered">
-            <h1 class="is-size-1"><b>Cookies: {{ +cookies.toFixed(1) }}</b></h1>
-            <h3 class="is-size-3"><b>CPS: {{ +cps.toFixed(1) }}</b></h3>
-            <figure class="image is-square m-5">
-                <img @click="cookies++" class="is-rounded" src="https://pngimg.com/uploads/cookie/cookie_PNG13656.png" />
-            </figure>
-        </div>
-        <div class="column is-7 has-background-info">Second column</div>
-        <div class="column is-2 has-background-warning">
-            <button class="button is-large is-fullwidth is-primary" v-for="building in buildings" :disabled="cookies<building.price" @click="buyBuilding(building)" >
-                {{ building.name }} 🍪{{ building.price }} #{{ building.count }}
-            </button>
-        </div>
-    </div>
+	<div class="columns">
+		<div class="column is-3 has-background-primary has-text-centered">
+			<h1 class="is-size-1">
+				<b>Cookies: {{ +cookies.toFixed(1) }}</b>
+			</h1>
+			<h3 class="is-size-3">
+				<b>CPS: {{ +cps.toFixed(1) }}</b>
+			</h3>
+			<figure class="image is-square m-5">
+				<img @click="CookieClick" :src="currentImage" class="is-rounded" />
+			</figure>
+		</div>
+		<div class="column is-7 has-background-info">Something</div>
+		<div class="column is-2 has-background-warning">
+			<button
+				class="button is-large is-fullwidth is-primary"
+				v-for="building in buildings"
+				:disabled="cookies < building.price"
+				@click="buyBuilding(building)"
+			>
+				{{ building.name }} 🍪{{ building.price }} #{{ building.count }}
+			</button>
+			<button
+				class="button is-large is-fullwidth is-secondary"
+				v-for="buff in buffs"
+				:disabled="cookies < buff.price"
+				@click="buyBuff(buff)"
+			>
+				{{ buff.name }} 🖱️{{ buff.price }} #{{ buff.count }}
+			</button>
+		</div>
+	</div>
 </template>
--- webpack.config.js
+++ webpack.config.js
@@ -1,52 +1,68 @@
 import path from "path";
 import HtmlWebpackPlugin from "html-webpack-plugin";
-import { VueLoaderPlugin } from 'vue-loader';
+import { VueLoaderPlugin } from "vue-loader";
+
 export default {
-  entry: "./src/index.js",
-  output: {
-    filename: "main.js",
-    path: path.resolve(import.meta.dirname, "dist"),
-  },
-  devServer: {
-    static: {
-      directory: path.join(import.meta.dirname, "public"),
-    },
-    compress: true,
-    port: 9000,
-    historyApiFallback: true,
-    allowedHosts: "all",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.scss$/i,
-        use: [
-          "style-loader",
-          "css-loader",
-          {
-            loader: "sass-loader",
-            options: {
-              sassOptions: {
-                quietDeps: true,
-              },
-            },
-          },
-        ],
-      },
-      {
-        test: /\.vue$/,
-        loader: 'vue-loader'
-      }
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/index.html",
-    }),
-    new VueLoaderPlugin()
-  ],
+	entry: "./src/index.js",
+	output: {
+		filename: "main.js",
+		path: path.resolve(import.meta.dirname, "dist"),
+	},
+	devServer: {
+		static: {
+			directory: path.join(import.meta.dirname, "public"),
+		},
+		compress: true,
+		port: 9000,
+		historyApiFallback: true,
+		allowedHosts: "all",
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				use: ["style-loader", "css-loader"],
+			},
+			{
+				test: /\.(png|jpe?g|gif|svg)$/i,
+				type: "asset/resource",
+			},
+			{
+				test: /\.mp3$/,
+				use: [
+					{
+						loader: "file-loader",
+						options: {
+							name: "assets/[name].[hash:8].[ext]",
+						},
+					},
+				],
+			},
+			{
+				test: /\.scss$/i,
+				use: [
+					"style-loader",
+					"css-loader",
+					{
+						loader: "sass-loader",
+						options: {
+							sassOptions: {
+								quietDeps: true,
+							},
+						},
+					},
+				],
+			},
+			{
+				test: /\.vue$/,
+				loader: "vue-loader",
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: "./src/index.html",
+		}),
+		new VueLoaderPlugin(),
+	],
 };
